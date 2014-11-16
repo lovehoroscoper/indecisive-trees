7c47f0448deaa60b00e59df762ef48bc4655479a
bobae317 <bobae317@berkeley.edu>
2014-11-15 16:54:14 -0800

    Update cosine_similarity.py

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,10 +1,13 @@
-def vector_length(vector): 
+def vector_length(vector):
+    """vector_length({'pennies': 1, 'nickels': 2, 'dimes': 3, 'quarters': 4 })"""
     tot = 0 
     for term, count in vector.items(): 
         tot += count * count 
     return sqrt(tot) 
  
 def dot_product(vecA, vecB):
+    """dot_product({'pennies': 2, 'nickles', 4}, {'pennies': 1, 'nickles': 2})
+        10"""
     tot = 0
     for term in vecA:
         if term in vecB:
@@ -12,5 +15,6 @@ def dot_product(vecA, vecB):
     return total
  
 def cos_similarity(query, doc):
+    """single value between 0 and 1"""
   total = dot_product(query, doc)
   return float(total) / ( vector_length(query) * vector_length(doc) )
