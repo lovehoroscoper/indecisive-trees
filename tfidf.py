793b8e030fa470b169d15619c9c3830d926ee751
bobae317 <bobae317@berkeley.edu>
2014-11-15 16:28:07 -0800

    Update tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -23,3 +23,25 @@ def idf(token, documentList):
 def tfidf(token, document, documentList):
   return (tf(token,document) * idf(token,documentList))
 
+def get_tokens(id_val, filename):
+    """ Given (ID, filename), return the list of tokens associated with it"""
+    id_token_list = [line.strip() for line in open(filename)]
+    id_token = id_token_list[int(id_val)]
+    token_list = id_token.split()
+    tokenlist = token_list[1].split("|")
+    return tokenlist
+    
+def cal_tf_idf(ad_id):
+    instance_list = [line.strip() for line in open("instances.txt")]
+    for ins in instance_list:
+        if ins.split()[3] == ad_id:
+            idlist = ins.split()
+    tokens_query = get_tokens(idlist[7], 'queryid_tokensid.txt')
+    tokens_keyword = get_tokens(idlist[8], "purchasedkeywordid_tokensid.txt")
+    tokens_title = get_tokens(idlist[9], "titleid_tokensid.txt")
+    tokens_description = get_tokens(idlist[10], "descriptionid_tokensid.txt")
+    alltoken = tokens_query + tokens_keyword + tokens_title + tokens_description
+    for query in tokens_query:
+        dic = {}
+        dic[query]= tfidf(query, tokens_query, alltoken)
+        print dic
