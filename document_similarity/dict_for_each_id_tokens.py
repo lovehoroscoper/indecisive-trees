a21e7a1c5ad72018de98ca56d865566e43a31075
bobae317 <bobae317@berkeley.edu>
2014-11-17 15:57:35 -0800

    Delete dict_for_each_id_tokens.py

--- document_similarity/dict_for_each_id_tokens.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    dictionary ={}
-    line = line.strip()
-    tokens = line.split('\t')
-    tokenlist = tokens[1].split("|")
-    dictionary[tokens[0]] = tokenlist
-    print dictionary
