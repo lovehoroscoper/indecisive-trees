72b1193be4376500cf5597371e4ac8793f4142d2
bobae317 <bobae317@berkeley.edu>
2014-11-17 16:07:32 -0800

    Update get_similarity_index.py

--- document_similarity/get_similarity_index.py
+++ document_similarity/get_similarity_index.py
@@ -1,8 +1,10 @@
 from collections import Counter
 import math
 def get_dictionary_of_id_tokens_and_freq(filename):
-    """input: a text file
+    """ input: a text file
         output: a dictionary with unique id as a key and counter token as values
+        Example:
+         {'1': Counter({'75': 1, '1545': 1, '31': 1}), '0': Counter({'12731': 1})}
     author: Elizabeth Lee"""
     result_dict ={}
     for line in open(filename):
@@ -11,20 +13,18 @@ def get_dictionary_of_id_tokens_and_freq(filename):
         token_list = token_list.split("|")
         result_dict[id] = Counter(token_list)
     return result_dict
+
 dict_query = get_dictionary_of_id_tokens_and_freq('queryid_tokensid.txt')
-#output has a form of {'1': Counter({'75': 1, '1545': 1, '31': 1}) : '1' is id value and '75' is token and 1 means how many times this token appeared in a line
-#'0': Counter({'12731': 1}), '3': Counter({'518': 1, '1996': 1}),
-# '2': Counter({'383': 1}), '4': Counter({'4189': 1, '75': 1, '31': 1})}
 dict_keyword = get_dictionary_of_id_tokens_and_freq("purchasedkeywordid_tokensid.txt")
 dict_title = get_dictionary_of_id_tokens_and_freq("titleid_tokensid.txt")
 dict_description = get_dictionary_of_id_tokens_and_freq("descriptionid_tokensid.txt")
     
 def get_idf_dictionary(query_dictionary, keyword_dictionary, title_dictionary, description_dictionary):
     """
+    one idf(inverse document frequency) is needed for each token in all for documents (it can be used for all 4 documents)
     input: 4 dictionaries that has a form of Counter({'4189': 1, '75': 1, '31': 1}) 
     output: a dictionary with a token(word) as a key and idf as a value
     author: Elizabeth Lee"""
-    #one idf is needed for each token in all for documents (it can be used for all 4 documents)
     token_set = set()
     token_set.update(query_dictionary.keys())
     token_set.update(keyword_dictionary.keys())
