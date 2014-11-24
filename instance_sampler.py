70046026e5cf88a80dc98535e94a60b0f5ba91e3
Alan Kao <alan.kao@berkeley.edu>
2014-11-23 18:28:49 -0800

    add instance sampler script

---
+++ instance_sampler.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import random
+from os import sys
+
+PERCENTAGE = 0.12
+
+for line in sys.stdin:
+    line = line.strip()
+    fields = line.split('\t')
+
+    clicks = None
+    impressions = None
+    try:
+    	clicks = int(fields[0])
+    	impressions = int(fields[1])
+    except ValueError:
+    	continue
+
+    if impressions == 0:
+    	continue
+
+    try:
+    	userid = int(fields[11])
+    	if userid == 0:
+    		continue
+    except ValueError:
+    	pass
+
+    for i in range(impressions):
+    	if random.random() > PERCENTAGE:
+    		continue
+    	# positive instance == 1
+    	if random.random() < (float(clicks) / float(impressions)):
+    		print "1\t" + line
+    	else:
+    		print "0\t" + line
